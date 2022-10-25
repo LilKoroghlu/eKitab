95d71b9f7791625d759b422c4c38f2d792bd2007
LilKoroghlu <lil.koroghlu@gmail.com>
2022-10-26 00:05:51 +0400

    Add project files.

---
+++ eKitabWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using eKitabWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eKitabWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> optioins) : base(optioins)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ eKitabWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eKitabWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
